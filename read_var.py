7e17a2b4af89ee1e12b6f681d2ed686772c97cce
mcchri <112865223+mcchri@users.noreply.github.com>
2024-02-20 10:08:47 +0000

    Add files via upload

--- read_var.py
+++ read_var.py
@@ -3,6 +3,46 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import serial
+
+PID_MICROBIT = 516
+VID_MICROBIT = 3368
+TIMEOUT = 0.1
+
+def find_comport(pid, vid, baud):
+    ''' return a serial port '''
+    ser_port = serial.Serial(timeout=TIMEOUT)
+    ser_port.baudrate = baud
+    ports = list(list_ports.comports())
+    print('scanning ports')
+    for p in ports:
+        if (p.pid == pid) and (p.vid == vid):
+            print('found target device pid: {} vid: {} port: {}'.format(p.pid, p.vid, p.device))
+            ser_port.port = str(p.device)
+            return ser_port
+    return None
+
+ser_micro = find_comport(PID_MICROBIT, VID_MICROBIT, 115200)
+if not ser_micro:
+    print('microbit not found')
+else:    
+    ser_micro.open()
+
+def firebase_read_reference(ref1,aux,ser1):
+    source = 'microbit'
+    var1 = ser1.readline()
+    var1 = str(var1.decode('utf-8'))
+    if aux == 0:
+        ref1.update({str(int(time.time())):{'Light_level':var1, 'Location':source}})
+        print("Light_level:",var1)
+    elif aux == 1:
+        ref1.update({str(int(time.time())):{'Age':var1, 'Location':source}})
+        print("Age:",var1)
+    elif aux == 2:
+        ref1.update({str(int(time.time())):{'Score':var1, 'Location':source}})
+        print("Score:",var1)
+    else:
+        ref1.update({str(int(time.time())):{'Level':var1, 'Location':source}})
+        print("Level:",var1)
 # path to the private key
 cred = credentials.Certificate("C:/Users/19CTurean.ACC/Documents/comp-sci-c8d0a-firebase-adminsdk-yk4h3-55cfc3c52d.json")
 # URL to the database
@@ -15,22 +55,17 @@ ser.baudrate = 115200
 ser.port = "COM6"
 ser.open()
 
-ref = db.reference().child('Light')
-ref = db.reference().child('Age')
-ref = db.reference().child('Score')
-
-ref.update({'Description':'Biology123 class', 'id':'Biology123', 'title':'Biology 123'})
-#ref = db.reference().child('classes').child('Math123')
-#ref.update({'Description':'Math123 class', 'id':'Math123', 'title':'Math123'})
-
-#ref = db.reference().child('classes').child('Biology123')
-#ref.delete()
-
-
-
 while True:
-    light = ser.readline()
-    light = str(light.decode('utf-8'))
-    #ref = db.reference().child('Light')
-   # ref.update({'Description':light, 'id':light, 'title':light})
-    print(light)
+    iterator = 0
+    ref = db.reference().child('Light')
+    firebase_read_reference(ref,iterator,ser)
+    iterator += 1
+    ref = db.reference().child('Age')
+    firebase_read_reference(ref,iterator,ser)
+    iterator += 1
+    ref = db.reference().child('Score')
+    firebase_read_reference(ref,iterator,ser)
+    iterator += 1
+    ref = db.reference().child('Level')
+    firebase_read_reference(ref,iterator,ser)
+    #ref.delete()
